bf8dff44326f0c986e5e8940ecf2ec099d896d4a
Keydrain <Keydrain@users.noreply.github.com>
2015-04-08 10:37:48 -0600

    Preliminary Motor Code
    Added some preliminary motor code to colleciton.ino. Will change soon when actual motor testing can commence.

--- Collection.ino
+++ Collection.ino
@@ -3,10 +3,26 @@ Author: Keydrain
 */
  
 #include "Arduino.h"
+#include <Stepper.h>
+
+int in1Pin = 12;
+int in2Pin = 11;
+int in3Pin = 10;
+int in4Pin = 9;
+//int inPumpPin = 8; // This may be needed for the pump. Not sure...
+
+Stepper motor(4, in1Pin, in2Pin, in3Pin, in4Pin);
  
 void setup()
 {
+ 
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(in3PIn, OUTPUT);
+  pinMode(in4Pin, OUTPUT);
+ 
   Serial.begin(9600); 
+  motor.setSpeed(20); // 20 is a filler for the time being. 
 }
  
 void loop()
@@ -17,5 +33,9 @@ void loop()
   Serial.print(val1);
   Serial.print("\n");
   // Resolution of deciseconds
+  
+  /*
+  motor.step(1); // This is to move one step. Needs some logic to determine when to step. 
+  */
   delay(100);
 }
